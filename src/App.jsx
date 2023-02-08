b514a1bbd69b747e050cb4377d81de6eeb14f4a8
FedeVts <ramella93@gmail.com>
2023-02-08 15:26:09 -0300

    agregegando-cosas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,4 @@
-// import './App.css'
-// import React from 'react'
-// import Navbar from './components/NavBar/NavBar'
-// import Inicio from './pages/inicio'
 
-
-// const App = () => {
-//   return (
-//     <div className='App'>
-      
-//       <Navbar/>
-//       <Inicio/>
-
-//     </div>
-//   )
-// }
-
-// export default App
 import "../src/index.css";
 import Inicio from "./pages/Inicio"
 
